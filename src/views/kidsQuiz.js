ba939305ee12257108c27e596e1746cdb18987ce
Yosliana <yoslianaguilar@gmail.com>
2020-08-24 09:12:33 -0400

    Componente y vista trivia -capeta data archivo data.json - componente Quiz - llamado de componente Quiz en vista kidsQuiz

--- src/views/kidsQuiz.js
+++ src/views/kidsQuiz.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import data from '../data/data.json';
+import { Quiz } from '../components/Quiz';
+
 export const kidsQuiz = () =>{
+  const onOk = () =>{
+    alert("Respuesta Correcta")
+  }
+  const onError = () =>{
+    alert("Respuesta Incorrecta")
+    
+  }
   return(
     <div>
-    Pagina de Trivias para los Niños      
+    Pagina de Trivias para los Niños   
+      <Quiz onOk={onOk} onError={onError} questions={data.questions}/>
+
     </div>
   )
 }
