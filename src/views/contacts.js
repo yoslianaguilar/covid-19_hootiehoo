4de88f186e9568ca016a43254fafc42d8eb9004c
natalieechenique <natalie.terapias@gmail.com>
2020-08-24 12:16:16 -0400

    Arreglo rutas

--- src/views/contacts.js
+++ src/views/contacts.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Inicio.css';
 
 export const ContactsPage = () =>{
   return(
-    <div>
-    Pagina Contacto      
+    <div className ='contact'>
+      <h3>Contacto</h3> 
+    <p className='text'>Este es nuestro contacto</p>     
     </div>
   )
 }
