499ed85dc61e5ce8d0b2c653b9ed4b7ae41db8c4
Yosliana <yoslianaguilar@gmail.com>
2020-08-26 13:26:06 -0400

    cambios

--- src/routers/adultRouters.js
+++ src/routers/adultRouters.js
@@ -1,18 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+// import { Link } from "react-router-dom";
+import {Nav} from 'react-bootstrap'
 
 export const AdultRouters = () => {
   return (
   
-      <div className='buttons-cont' >
+<Nav defaultActiveKey="/adultHome" className="flex-column">
+  <Nav.Link href="/adultTips">Consejos</Nav.Link>
+  <Nav.Link eventKey="/adultNews">Noticias</Nav.Link>
+  <Nav.Link eventKey="/materialDownload">Material Descargable</Nav.Link>
+  
+</Nav>
+
+
+
+      /* <div className='buttons-cont' >
         <nav>
           <Link to="/adultTips"><button>Consejos</button></Link>
           <Link to="/adultNews"><button>Noticias</button></Link>
           <Link to="/materialDownload"><button>Material Descargable</button></Link>
         </nav>
        
-      </div>
+      </div> */
   
-  );
+  )
 }
