8e0406eec5adef1a2bb6740f17e9e2dd3c9697a2
natalieechenique <natalie.terapias@gmail.com>
2020-08-27 12:51:36 -0400

    Estilos 1ras y 2das rutas, inicio niños con personajes

--- src/views/about.js
+++ src/views/about.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import '../css/about.css';
-import icono1 from "../Img/Icono_Demo_1.png";
+import icono1 from "../Img/Icono_Demo_2.png";
 
 export const AboutPage = () =>{
   return(
     <div className='about'>
-      <div className='icono2-content'>
+        <div className ='info-cont'>
+      <div className='icono2-cont'>
          <img src={icono1} className='icono2' alt="icono" width='50px'/> 
-      <h3>Quiénes somos</h3>
-      </div> 
+      <h3>¿Quiénes somos?</h3>
     <p className='text'>Somos un equipo que busca desarrollar el potencial de las personas y empresas entregando soluciones tecnológicas creativas 
     y motivadoras para Capacitación y Marketing, que faciliten la Comunicación y el Aprendizaje.</p>     
+    </div>
+    </div> 
     </div>
   )
 }
