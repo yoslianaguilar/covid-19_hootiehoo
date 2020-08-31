14cb1fdf243027cda7ad6ecb10536a551804cd63
natalieechenique <natalie.terapias@gmail.com>
2020-08-31 10:59:41 -0400

    detalle arreglo

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Videos = () => {
     return (
         <div className =''>
-            <h4>Conoce y sigue las aventuas de Puma Espuma y el Escuadrón Prevención </h4>
+            <h4>Conoce y sigue las aventuras de Puma Espuma y el Escuadrón Prevención </h4>
 
             <p>Una gran aventura en un mundo muy pequeño</p>
             <iframe width="300" height="215" src="https://www.youtube.com/embed/tuILs6Zedco" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
