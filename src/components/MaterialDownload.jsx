66668553cce0d52fd97b0c47ff4a0039eb9b65b5
alejandra <alejandra.colicheo17@gmail.com>
2020-09-01 10:07:53 -0300

    cambio de botón a portadas de material descargable

--- src/components/MaterialDownload.jsx
+++ src/components/MaterialDownload.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import portada from '../Img/portada.png'
 
 export const MaterialDownload = () => {
     return (
@@ -7,32 +8,32 @@ export const MaterialDownload = () => {
 
             <div className='downbox'>
     <h5>Modulo I - Tema 1 - "¿Qué está pasando?"</h5>
-       <button><a href="https://drive.google.com/file/d/1wYdXjI50S1vLI-DyP-8FxTvwoq69WjDT/view?usp=sharing" download="HH_GanemosleAlVirus_PDF_M1_T1.pdf">Haz Click Aquí</a></button>
-       </div>
+    <a href="https://drive.google.com/file/d/1wYdXjI50S1vLI-DyP-8FxTvwoq69WjDT/view?usp=sharing" download="HH_GanemosleAlVirus_PDF_M1_T1.pdf"><img class="projectImg" src={portada} width='120px' alt="project portada"></img></a>
+    </div>
     
        <div className='downbox'>
     <h5>Modulo I - Tema 2 - "¿Qué es el COVID-19?"</h5>
-    <button><a href="https://drive.google.com/file/d/1Vqu1-Dk2vL_BOULappWrr_i6o-gIAno3/view?usp=sharing" download="HH_GanemosleAlVirus_PDF_M1_T2.pdf">Haz Click Aquí</a></button>
+    <a href="https://drive.google.com/file/d/1Vqu1-Dk2vL_BOULappWrr_i6o-gIAno3/view?usp=sharing" download="HH_GanemosleAlVirus_PDF_M1_T2.pdf"><img class="projectImg" src={portada} width='120px' alt="project portada"></img></a>
     </div>
     
     <div className='downbox'>
     <h5>Modulo II - Tema 1 - "Cuidando las relaciones en cuarentena"</h5>
-    <button><a href="https://drive.google.com/file/d/1Y5VbiQVtjX3ndVwuj14dV21ok6JRiIek/view?usp=sharing" download="HH_GanemosleAlVirus_PDF_M2_T1.pdf">Haz Click Aquí</a></button>
+    <a href="https://drive.google.com/file/d/1Y5VbiQVtjX3ndVwuj14dV21ok6JRiIek/view?usp=sharing" download="HH_GanemosleAlVirus_PDF_M2_T1.pdf"><img class="projectImg" src={portada} width='120px' alt="project portada"></img></a>
     </div>
     
     <div className='downbox'>
     <h5>Modulo II - Tema 2 - "¿Cómo cambia la mente de los niños y niñas?"</h5>
-    <button><a href="https://drive.google.com/file/d/1iQQVfj_StSG7vdT7EhgSka8Kp2p76nbL/view?usp=sharing" download="HH_GanemosleAlVirus_PDF_M2_T2.pdf">Haz Click Aquí</a></button>
+    <a href="https://drive.google.com/file/d/1iQQVfj_StSG7vdT7EhgSka8Kp2p76nbL/view?usp=sharing" download="HH_GanemosleAlVirus_PDF_M2_T2.pdf"><img class="projectImg" src={portada} width='120px' alt="project portada"></img></a>
     </div>
     
     <div className='downbox'>
     <h5>Modulo II - Tema 3 Psicohigiene: - "El cuidado de uno mismo y de los demás"</h5>
-    <button><a href="https://drive.google.com/file/d/1asbFtRJcnq92RB0e1FfvBfNMqxY3i3Mf/view?usp=sharing" download="HH_GanemosleAlVirus_PDF_M2_T3.pdf">Haz Click Aquí</a></button>
+    <a href="https://drive.google.com/file/d/1asbFtRJcnq92RB0e1FfvBfNMqxY3i3Mf/view?usp=sharing" download="HH_GanemosleAlVirus_PDF_M2_T3.pdf"><img class="projectImg" src={portada} width='120px' alt="project portada"></img></a>
     </div>
 
     <div className='downbox'>
     <h5>Modulo III - Tema 1 - "Instalación y control de hábitos saludables para la inmunidad física y mental"</h5>
-    <button><a href="https://drive.google.com/file/d/1gWHMc8cjJZuOB3D0eQ1tErG9HQSkixK6/view?usp=sharing" download="HH_GanemosleAlVirus_PDF_M2_T3.pdf">Haz Click Aquí</a></button>    
+    <a href="https://drive.google.com/file/d/1gWHMc8cjJZuOB3D0eQ1tErG9HQSkixK6/view?usp=sharing" download="HH_GanemosleAlVirus_PDF_M2_T3.pdf"><img class="projectImg" src={portada} width='120px' alt="project portada"></img></a>  
     </div>
     </div>
     )
