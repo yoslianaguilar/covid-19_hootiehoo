7f31a95764e1e5649d27849131232fab99d290e3
natalieechenique <natalie.terapias@gmail.com>
2020-09-01 09:31:28 -0400

    Contenedor a videos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { AppRouter } from './routers/router';
 import { Footer } from './components/footer';
+// import Change from './components/Change';
+
 
 function App() {
   return (
     <div className="App">
-
+    
     <AppRouter />
+    {/* <Change /> */}
     <Footer />
+   
     </div>
   );
 }
---
+++ src/components/Change.js
@@ -0,0 +1,53 @@
+// import React, { useState, Component } from 'react';
+// import '../css/index.css';
+// // import styled from 'styled-components';
+  
+
+
+// class Change extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       bgColor: ""
+//     };
+//   }
+
+//   body = e => {
+//     this.setState({
+//       bgColor: "red"
+//     });
+//   };
+
+//   render() {
+//     return (
+
+
+
+
+      
+//         <div className="body">
+//           <div
+//             className="bodyCss"
+//             style={{ backgroundColor: this.state.bgColor }}
+//             onClick={this.body}
+//           >
+//             Click Me!
+//             {' | '}
+//             <div
+//             className="bodyCss"
+//             style={{ clearBackgroundColor: this.state.bgColor }}
+//             onClick={this.body}
+//           >
+//            Reset
+        
+//           </div>
+//         </div>
+
+//         </div>
+     
+//     );
+//   }
+// }
+
+
+// export default Change;
