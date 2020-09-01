756b726f460b3ab3330cc6e5aedff8f65ffd1eb7
natalieechenique <natalie.terapias@gmail.com>
2020-09-01 09:31:28 -0400

    Contenedor a videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,18 +2,29 @@ import React from 'react';
 
 export const Videos = () => {
     return (
-        <div className =''>
+        <div className ='videos'>
             <h4>Conoce y sigue las aventuras de Puma Espuma y el Escuadrón Prevención </h4>
 
+            <div className ='videos-cont'>
             <p>Una gran aventura en un mundo muy pequeño</p>
+             {/* eslint-disable-next-line  */}
             <iframe width="300" height="215" src="https://www.youtube.com/embed/tuILs6Zedco" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           
             <p>La corona es nuestra</p>
+                     {/* eslint-disable-next-line  */}
             <iframe width="300" height="215" src="https://www.youtube.com/embed/G2BHb6kWnRc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            
             <p>¿Te gusto la canción Puma Espuma y sus amigos? Te invitamos a aprenderla y cantarla junto a nosotros y todos tus amigos</p>
+             {/* eslint-disable-next-line  */}
             <iframe width="300" height="215" src="https://www.youtube.com/embed/9skA9MFlcs4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           
             <p>¡¡Mira como muchos niños y niñas cantan con Puma Espuma!!</p>
+          {/* eslint-disable-next-line  */}
             <iframe width="300" height="2" src="https://www.youtube.com/embed/zcG4TeFRSBA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        
         </div>
+        </div>
+
 
 
     )}
